477fb14ea41ea7c0b914ef288487b804802466a7
aly-abdelkader-iut-cse-4301 <38514887+aly-abdelkader-iut-cse-4301@users.noreply.github.com>
2020-11-21 02:13:32 +0600

    Initial commit and folder structuration

---
+++ src/actions/shared.js
@@ -0,0 +1,14 @@
+import { setAuthedUser } from '../actions/authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
+
+export function handleInitialData () {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, tweets }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receiveTweets(tweets))
+        dispatch(setAuthedUser(AUTHED_ID))
+      })
+  }
+} 
